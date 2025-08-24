5728a0f68c58e19e8bc0c9b00f4c3ff513cc2ac5
srinivasan0077 <srinivasandhandapani071201@gmail.com>
2025-08-24 21:28:18 +0530

    First commit of intelligent email categorization system

---
+++ src/main/java/com/veprojects/inboxiq/InboxiqApplication.java
@@ -0,0 +1,13 @@
+package com.veprojects.inboxiq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InboxiqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InboxiqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/veprojects/inboxiq/InboxiqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.veprojects.inboxiq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InboxiqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
